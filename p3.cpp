f750359e4b073025114f8dc0d188976d8e3feed8
rudra <141342026+rudra0102@users.noreply.github.com>
2023-11-03 17:42:52 +0530

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class A
+{
+	public:
+    void fun();
+};
+
+void A::fun() { cout<<"Function Called Succesfully"; }
+
+int main()
+{
+    A a;
+    a.fun();
+ 
+    return 0;
+}
